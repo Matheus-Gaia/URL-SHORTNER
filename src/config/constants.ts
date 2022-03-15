e3bfdd477e1580f51b22ff203ee377f3f5ba581b
Matheus Gaia <87877640+Matheus-Gaia@users.noreply.github.com>
2022-03-15 01:10:56 -0300

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const config = {
     API_URL: 'http://localhost:3000',
     MONGO_CONNECTION: 
-        'mongodb+srv://mnunes:9wUj4SlY9fdeSPbN@url-shortner.lu8ds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        'mongodb+srv://mnunes:<password>@url-shortner.lu8ds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
